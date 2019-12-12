3c68d2f092d1447e05168e680bb81181ab834c8a
koukyo1994 <arabiannight1994@yahoo.co.jp>
2019-12-12 17:49:46 +0100

    Initial Commit

---
+++ iOS-note-v1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-note-v1
+//
+//  Created by 荒居秀尚 on 12.12.19.
+//  Copyright © 2019 荒居秀尚. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
