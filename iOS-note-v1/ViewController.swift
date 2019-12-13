d5d02246ee881ac03d2517bd7f69c6b3aa0e04a1
koukyo1994 <arabiannight1994@yahoo.co.jp>
2019-12-13 17:00:33 +0100

    WIP

--- iOS-note-v1/ViewController.swift
+++ iOS-note-v1/ViewController.swift
@@ -7,14 +7,64 @@
 //
 
 import UIKit
+import PencilKit
+import Vision
 
 class ViewController: UIViewController {
+    
+    var canvasView: PKCanvasView?
+
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let canvas = PKCanvasView(frame: self.imageView.frame)
+        view.addSubview(canvas)
+        canvas.tool = PKInkingTool(.pen, color: .black, width: 20)
+        
+        self.canvasView = canvas
+        
+        if let window = UIApplication.shared.windows.first {
+            if let toolPicker = PKToolPicker.shared(for: window) {
+                toolPicker.addObserver(canvas)
+                toolPicker.setVisible(true, forFirstResponder: canvas)
+                canvas.becomeFirstResponder()
+            }
+        }
     }
 
-
+    
+    @IBAction func detectText(_ sender: Any) {
+        let image = canvasView!.drawing.image(
+            from: canvasView!.bounds,
+            scale: 1.0)
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        let cgImage = image.cgImage
+        let request = VNRecognizeTextRequest(completionHandler: self.detectTextHandler)
+        
+        request.recognitionLevel = .accurate
+        request.recognitionLanguages = ["en_US"]
+        request.usesLanguageCorrection = false
+        
+        let requests = [request]
+        let imageRequestHandler = VNImageRequestHandler(cgImage: cgImage!, options: [:])
+        try! imageRequestHandler.perform(requests)
+    }
+    
+    func detectTextHandler(request: VNRequest?, error: Error?) {
+        guard let observations = request?.results as? [VNRecognizedTextObservation] else {
+            return
+        }
+        
+        for observation in observations {
+            let candidates = 1
+            guard let bestCandidate = observation.topCandidates(candidates).first else {
+                continue
+            }
+            print(bestCandidate.string)
+            print(bestCandidate.confidence)
+        }
+    }
 }
 
